f5d7732f72a12f19f935b7c508069710ad46b36b
Valera21v <valera11.mail@mail.ru>
2022-10-26 13:48:24 +0300

    Редактирование кода

--- first/nums.py
+++ first/nums.py
@@ -5,6 +5,7 @@ def summ(a,b):
 def minus(a,b):
     return a-b
 
+
 def division(a,b):
     return a/b
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from first.nums import summ
 
+
 if __name__ == '__main__':
-    print('Is main '+str(summ(13, 2)))
+    print('Is main '+str(summ(10, 2)))
     
